50664acf50666cd2c508eefd96e9d9e6302ce0ff
vishnukr-123 <42140254+vishnukr-123@users.noreply.github.com>
2018-08-06 16:23:23 +0530

    Create game.php

---
+++ game.php
@@ -0,0 +1,107 @@
+<head>
+<style type="text/css">
+	body
+	{
+		background-image: url("aa.jpeg");
+		background-repeat: no-repeat;
+		background-size: 100%;
+	}
+	.first
+	{
+		width: 100%;
+		height: 30px;
+		margin-left: 0px;
+		margin-top: 0px;
+		background-color: #808080;
+	}
+	li {
+    display: inline;
+}
+ul {
+	margin-top: 0px;
+	margin-left: 0px;
+    list-style-type: none;
+    margin: 0px;
+    padding: 0px;
+    overflow: hidden;
+    background-color: #333;
+}
+
+li {
+    float: left;
+}
+
+li a {
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+}
+
+
+li a:hover {
+    background-color: #00FFFF;
+}
+.active {
+    background-color: #4CAF50;
+}
+.aa
+{
+	margin-top: 80px;
+	margin-left: 40px;
+}
+h1
+{
+	margin-top: -135px;
+	margin-left: 100px;
+	text-align: center;
+}
+h2
+{
+	margin-top: 50px;
+	margin-left: 100px;
+	text-align: center;
+	size:50px;
+}
+.ff
+{
+	margin-left: 120px;
+}
+</style>
+</head>
+<body>
+		<ul>
+	  		<li><a href="#Description"><font color="#008080">Description</font></a></li>
+	  		<li><a href="#Products"><font color="#008080">Products</font></a></li>
+	  		<li><a href="#Coming soon"><font color="#008080">Coming soon</font></a></li>
+	  		<li style="float:right"><a class="active" href="#about">About</a></li>
+		</ul>
+		<img  class="aa" src="g.jpeg" width="150px" height="120px"></img>
+	<h1><font color="#FF0000"><center><b>TOP RATED GAMES COLLECTION</b></center></font></h1>
+	<br />
+	<br />
+	<h2><font color="#008080"><center><b>LEVEL UP YOUR GAMING EXPERIENCE</b></center></font></h2>
+	<br />
+	<br />
+	
+	<dl class="ff"><font color="#800000">Description</font>
+	<br />
+	<br />
+		<dt><font color="#FFFF00">Best Gaming Products</font></dt>
+		<dd>nihfiueufoev uuy9utpeiropg eoigerteu9fiepor eoigerygergi</dd>
+		<br />
+		<br />
+		<dt><font color="#FFFF00">Awesome Looking Rig </font></dt>
+		<dd>nihfiueufoev uuy9utpeiropg eoigerteu9fiepor eoigerygergi</dd>
+		<br />
+		<br />
+		<dt><font color="#FFFF00">Delivered at your doorstep </font></dt>
+		<dd>nihfiueufoev uuy9utpeiropg eoigerteu9fiepor eoigerygergi</dd>
+	</dl>
+	<br />
+	<br />
+	<center>Enter e-mail:
+	<input type="text" value=""></input>
+	<input type="button" value="place order"></input></center>
+</body>
